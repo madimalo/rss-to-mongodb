75b6f5062eb9fb7e065d2a6a5bba977afd571bcf
Leon Qi <qi.leon@gmail.com>
2012-12-31 02:10:10 -0800

    Added config.js to manage source websites and categories.

--- feedinsert.js
+++ feedinsert.js
@@ -1,37 +1,33 @@
-var feedparser = require('feedparser');
-var mongo = require("mongodb");
-var host = "127.0.0.1";
-var port = 27017;
-var db = new mongo.Db("newsproject", new mongo.Server(host, port, {}), {safe:false});
+var feedparser = require('feedparser'),
+    cheerio = require('cheerio'),
+	mongo = require('mongodb'),
+	config = require('./config.js'),
+	host = "127.0.0.1",
+	port = 27017,
+	db = new mongo.Db("news", new mongo.Server(host, port, {}), {safe:false});
 
-var url0 = 'http://sports.espn.go.com/espn/rss/nba/news';
-var url1 = 'http://tech2ipo.com/feed';//tech
-var url2 = 'http://rss.sina.com.cn/news/marquee/ddt.xml';//news
-var url3 = 'http://cn.nowness.com/issue/syndicate?output=rss' //fashion
-var url4 = 'http://www.ftchinese.com/rss/feed';//finance
-var url5 = 'http://feed.mtime.com/movienews.rss';//movie
-var url6 = 'http://sports.163.com/special/00051K7F/rss_sportslq.xml';//sports
-
-var urllist = [
-	'http://sports.espn.go.com/espn/rss/nba/news',
-	'http://tech2ipo.com/feed',
-	'http://rss.sina.com.cn/news/marquee/ddt.xml',
-	'http://cn.nowness.com/issue/syndicate?output=rss',
-	'http://www.ftchinese.com/rss/feed',
-	'http://feed.mtime.com/movienews.rss',
-	'http://sports.163.com/special/00051K7F/rss_sportslq.xml'];
 
 function callback (error, meta, articles){
-  if (error) console.error(error);
-  else {
+  if (error) {
+  	console.error(error);
+  } else {
 
 	db.open(function(error){
+
 		console.log("connected " + host + ":" + port);
 
 		db.collection("story", function(error, collection){
 			
 		    articles.forEach(function (article){
+
+  				var $ = cheerio.load(article.description);
+ 
+			    //console.log($('img').first().attr('src') + '\n');
+			    var img = $('img').first().attr('src');
+
+
 				// console.log('source: ' + meta.title);
+				// console.log('meta link: ' + meta.link);
 				// console.log('title: ' + article.title);
 				// console.log('link: ' + article.link);
 				// console.log('summary: ' + article.summary);//discription
@@ -41,7 +37,8 @@ function callback (error, meta, articles){
 				// console.log('author: ' + article.author);
 				// console.log('comments: ' + article.comments);
 				// console.log('category: ' + "tech");   
-				// console.log('*********************successfully inserted!\n');			
+				// console.log('*********************successfully inserted!\n');
+			
 				collection.insert({
 					source: meta.title,
 					title: article.title,
@@ -52,7 +49,8 @@ function callback (error, meta, articles){
 					guid: article.guid,
 					author: article.author,	
 					comments: article.comments,
-					category: "spt"					
+					category: config.category[meta.link],
+					image: img				
 				}, function(){
 					console.log(article.guid + " successfully inserted!");					
 				});					
@@ -64,7 +62,10 @@ function callback (error, meta, articles){
   }//else
 }//callback
 
-//feedparser.parseUrl(url6, callback);
-for(var i = 0; i <urllist.length; i++){
-	feedparser.parseUrl(urllist[i], callback);
-}
+feedparser.parseUrl(config.sites.tech.url1, callback);
+// for(var i = 0; i <urllist.length; i++){
+// 	feedparser.parseUrl(urllist[i], callback);
+// }
+
+//console.log(config.divimg["http://www.ifanr.com"]);
+//console.log(config.sites.tech.link3);
